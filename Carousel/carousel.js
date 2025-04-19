27dab8d22d7723af3dfd4add080f72c22f9f24e8
OctetularZ <taiabayomi15@gmail.com>
2025-04-19 22:42:43 +0100

    Almost finished carousel

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -7,3 +7,8 @@ function arrowTransitionOut(arrow) {
   arrow.classList.remove("bi-arrow-down-circle")
   arrow.classList.add("bi-arrow-down-circle-fill")
 }
+
+function goForward(arrow) {
+  let sl = document.getElementById("lp")
+  $('lp').scrollRight(400);
+}
