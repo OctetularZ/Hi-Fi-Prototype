477bb02644c84fc434e07be191f0e016e1239c78
OctetularZ <taiabayomi15@gmail.com>
2025-04-19 02:45:27 +0100

    first commit

---
+++ Carousel/carousel.js
@@ -0,0 +1,9 @@
+function arrowTransitionOn(arrow) {
+  arrow.classList.remove("bi-arrow-down-circle-fill")
+  arrow.classList.add("bi-arrow-down-circle")
+}
+
+function arrowTransitionOut(arrow) {
+  arrow.classList.remove("bi-arrow-down-circle")
+  arrow.classList.add("bi-arrow-down-circle-fill")
+}
