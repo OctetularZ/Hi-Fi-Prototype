7f59e77f711a1ad7af161ba177fcf91ba55b8285
OctetularZ <taiabayomi15@gmail.com>
2025-05-07 17:20:46 +0100

    Added base HTML, CSS and JS to login and register pages

---
+++ Register/register.js
@@ -0,0 +1,23 @@
+// Navigation menu
+
+const nav = document.querySelector('.nav-links')
+const openNavBtn = document.querySelector('#nav-toggle-open')
+const closeNavBtn = document.querySelector('#nav-toggle-close')
+
+
+const openNav = () => {
+  nav.style.display = 'flex';
+  openNavBtn.style.display = 'none';
+  closeNavBtn.style.display = 'inline-block';
+}
+
+openNavBtn.addEventListener('click', openNav)
+
+
+const closeNav = () => {
+  nav.style.display = 'none';
+  openNavBtn.style.display = 'inline-block';
+  closeNavBtn.style.display = 'none';
+}
+
+closeNavBtn.addEventListener('click', closeNav)
